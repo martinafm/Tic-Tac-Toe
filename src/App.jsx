015771fbc347f8a7d601aa523511da31b543bc25
Martyna <110538495+martinafm@users.noreply.github.com>
2024-08-13 13:52:25 +0200

    player functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Player from "./components/Player";
 function App() {
-  
-
-  return (
-    <p>Coming soon...</p>
-  )
+	return (
+		<main>
+			<div id="game-container">
+				<ol id="players">
+				<Player initialName='Player1' symbol="X" />
+				<Player initialName='Player2' symbol="O" />
+				</ol>
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
